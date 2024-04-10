96cc19a1f301bd6e22381947103859c066550f2c
XENOVAI-PC\alex5 <margraff.alexandre@gmail.com>
2024-04-10 23:10:29 +0200

    feat(layout): :recycle: wrap component in a Layout component with global page style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { HttpClient } from '@angular/common/http';
 import { MainComponent } from '../section/main/main.component';
+import { LayoutComponent } from '../layout/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, LayoutComponent, MainComponent],
   providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
